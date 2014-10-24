0fd91343bb28e01ea691f9d7d834c930c0e212a2
Aaron McCloud <mcclouda@gmail.com>
2014-10-24 12:22:33 -0700

    Got second tier drop down working

---
+++ js/dropdown.js
@@ -0,0 +1,47 @@
+// https://gist.github.com/ludder/4226288
+
+/*
+Element to slide gets the following CSS:
+    max-height: 0;
+    opacity: 0;
+    overflow: hidden;
+    transition: max-height 0.4s ease 0s;
+*/
+ 
+/**
+ * Like jQuery's slideDown function - uses CSS3 transitions
+ * @param  {Node} elem Element to show and hide
+ */
+
+function down(a) {
+  var x = document.querySelectorAll(a);
+  for (var i = x.length - 1; i >= 0; i--) {
+    x[i].style.maxHeight = '1000px';
+  };
+};
+
+function up(a) {
+  var x = document.querySelectorAll(a);
+  for (var i = x.length - 1; i >= 0; i--) {
+    x[i].style.maxHeight = '0';
+  };
+};
+
+
+ 
+/**
+ * Call once after timeout
+ * @param  {Number}   seconds  Number of seconds to wait
+ * @param  {Function} callback Callback function
+ */
+function once (seconds, callback) {
+  var counter = 0;
+  var time = window.setInterval( function () {
+    counter++;
+    if ( counter >= seconds ) {
+      callback();
+      window.clearInterval( time );
+    }
+  }, 400 );
+}
+
