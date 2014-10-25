73832f3c961ce24d27a9a75857553503625a128e
Aaron McCloud <mcclouda@gmail.com>
2014-10-25 14:21:44 -0700

    Got 2 tier dropdown, dynamically generated with JS, working.

--- js/dropdown.js
+++ js/dropdown.js
@@ -22,4 +22,22 @@ function up(a) {
 };
 
 
- 
+// Sandbox
+
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var kittens  = ["Kitten Option #1", "Kitten Option #2", "Kitten Option #3", "Kitten Option #4", "Kitten Option #5"];
+var puppies  = ["pug", "boxer", "golden retriever", "doberman", "hound of the baskervilles"];
+var birds    = ["golden eagle", "thrush", "bird of paradise", "parakeet", "raven"];
+var fish     = ["mahi-mahi", "saba", "sake", "Albacore", "Blue-fin"];
+var dino = ["ankylosaurus", "T-rex", "protoceratops", "quetzalcoatlus", "velociraptor"];
+
+function listGen(listRankAbc, array, elementId) {
+  list = "";
+  var i;
+  for (i = 0; i < array.length; i++) {
+    list += "<li class=\"secondTier " + listRankAbc + "\">" + array[i] + "</li>";
+  };
+document.getElementById(elementId).innerHTML = list;
+console.log(list);
+};
