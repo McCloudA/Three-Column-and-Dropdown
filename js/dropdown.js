0ce784710ea729759faf2caa6a6c5d5a25251a17
Aaron McCloud <mcclouda@gmail.com>
2014-10-25 19:38:05 -0700

    Perfected dropdown JS w/out extra ABC classes, tightened JS code.

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,22 +1,32 @@
-
-function down(a, b, c) {
-  var x = document.querySelectorAll(a);
-  for (var i = x.length - 1; i >= 0; i--) {
-    x[i].style.maxHeight = '1000px';
-    x[i].style.top = b;
-    x[i].style.left = c;
-  };
-};
+// Dropdown Animations
 
 function up(a) {
-  var x = document.querySelectorAll(a);
-  for (var i = x.length - 1; i >= 0; i--) {
-    x[i].style.maxHeight = '0';
+  var listClasses = document.querySelectorAll(a);
+  for (var i = listClasses.length - 1; i >= 0; i--) {
+    listClasses[i].style.maxHeight = '0';
   };
 };
 
+function down(elementInfo, topPosition, leftPosition) {
+  if (elementInfo == ".firstTier") {
+    var x = document.querySelectorAll(elementInfo);
+    for (var i = x.length - 1; i >= 0; i--) {
+      x[i].style.maxHeight = '1000px';
+      x[i].style.top = topPosition;
+      x[i].style.left = leftPosition;
+    };
+  } else {
+    var listArray = document.getElementById(elementInfo).childNodes;
+    var arrLength = listArray.length;
+    for (var i = 0; i < arrLength; i++) {
+      listArray[i].style.maxHeight = '1000px';
+      listArray[i].style.top = topPosition;
+      listArray[i].style.left = leftPosition;
+    };
+  };
+};
 
-// Sandbox
+// Second Tier Dropdown List Generator
 
 var kittens  = ["Kitten Option #1", "Kitten Option #2", "Kitten Option #3", "Kitten Option #4", "Kitten Option #5"];
 var puppies  = ["pug", "boxer", "golden retriever", "doberman", "hound of the baskervilles"];
@@ -24,11 +34,11 @@ var birds    = ["golden eagle", "thrush", "bird of paradise", "parakeet", "raven
 var fish     = ["mahi-mahi", "saba", "sake", "Albacore", "Blue-fin"];
 var dino = ["ankylosaurus", "T-rex", "protoceratops", "quetzalcoatlus", "velociraptor"];
 
-function listGen(listRankAbc, array, elementId) {
+function listGen(array, elementId) {
   list = "";
   var i;
   for (i = 0; i < array.length; i++) {
-    list += "<li class=\"secondTier " + listRankAbc + "\">" + array[i] + "</li>";
+    list += "<li class=\"secondTier\">" + array[i] + "</li>";
   };
-document.getElementById(elementId).innerHTML = list;
+  document.getElementById(elementId).innerHTML = list;
 };
